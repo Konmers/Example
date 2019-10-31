a5a43edaa81011875f1302516834d5995e4a6f42
Dliveaman <1916794877@qq.com>
2019-10-31 08:40:31 +0800

    add

--- demos/App.js
+++ demos/App.js
@@ -28,11 +28,14 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import Video from 'react-native-video';
+ 
 const App: () => React$Node = () => {
   return (
     <>
     <View>
-      <ActivityIndicator style={{margin:20}}></ActivityIndicator>
+      <ActivityIndicator style={{margin: 20}} color='red' size='large'></ActivityIndicator>
+      <ActivityIndicator style={{margin: 20}} color='blue' size='small'></ActivityIndicator>
       <Text style={{fontSize:20,textAlign:"center"}}>
         贼 nice 了
       </Text>
@@ -46,6 +49,24 @@ const App: () => React$Node = () => {
         style = {{width: 100,height: 100}}
         source= {require('./src/public/Iamge/10.jpg')}
       />
+      <Video
+        // ref={(ref: Video) => { //方法对引用Video元素的ref引用进行操作
+        //     this.video = ref
+        // }}
+        source = {require('./src/public/Iamge/test2.mp4')}
+        // rate={this.state.rate}//播放速率
+        // paused={this.state.paused}//暂停
+        // volume={this.state.volume}//调节音量
+        // muted={this.state.muted}//控制音频是否静音
+        // resizeMode={this.state.resizeMode}//缩放模式
+        // onLoad={this.onLoad}//加载媒体并准备播放时调用的回调函数。
+        // onProgress={this.onProgress}//视频播放过程中每个间隔进度单位调用的回调函数
+        // onEnd={this.onEnd}//视频播放结束时的回调函数
+        // onAudioBecomingNoisy={this.onAudioBecomingNoisy}//音频变得嘈杂时的回调 - 应暂停视频
+        // onAudioFocusChanged={this.onAudioFocusChanged}//音频焦点丢失时的回调 - 如果焦点丢失则暂停
+        // repeat={false}//确定在到达结尾时是否重复播放视频。
+        style={styles.backgroundVideo}
+      />
       <TextInput style={{height:40,margin:10,borderColor:'gray',borderWidth:1}} keyboardType='numeric' secureTextEntry={true} >
       </TextInput>
       <Button
@@ -57,89 +78,60 @@ const App: () => React$Node = () => {
         accessibilityLabel="Learn more about this purple button"
       />
     </View>
-    
-      {/* <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View> 
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView> */}
     </>
   );
 };
-
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  loading: {
+    margin:20,
   },
-  engine: {
+  backgroundVideo: {
     position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
     right: 0,
+    flex:1,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+})
+
+// const styles = StyleSheet.create({
+//   scrollView: {
+//     backgroundColor: Colors.lighter,
+//   },
+//   engine: {
+//     position: 'absolute',
+//     right: 0,
+//   },
+//   body: {
+//     backgroundColor: Colors.white,
+//   },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//     color: Colors.black,
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//     color: Colors.dark,
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+//   footer: {
+//     color: Colors.dark,
+//     fontSize: 12,
+//     fontWeight: '600',
+//     padding: 4,
+//     paddingRight: 12,
+//     textAlign: 'right',
+//   },
+// });
 
 export default App;
